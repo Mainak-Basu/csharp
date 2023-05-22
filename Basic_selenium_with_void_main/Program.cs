ce9544b493a15b2375f770b5078702046f755400
mainak basu <mainakbasu4@gmail.com>
2023-05-22 10:09:23 +0530

    first commit

---
+++ Basic_selenium_with_void_main/Program.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Basic_selenium_with_void_main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            Thread.Sleep(2000);
+            IWebElement element = driver.FindElement(By.Name("q"));
+            //Thread.Sleep(2000);
+            element.SendKeys("fleek it solutions");
+            Thread.Sleep(5000);
+            driver.FindElement(By.XPath("//span[contains(text(),'Fleek')]")).Click();
+            Thread.Sleep(5000);
+            driver.FindElement(By.XPath("//h3[contains(text(),'Top')]")).Click();
+            Thread.Sleep(8000);
+            driver.Close();      
+        }
+    }
+}
